1b2e2efc5cca5cfbd2f710f7db24a0d3a125bb50
ElPablones <79945710+ElPablones@users.noreply.github.com>
2026-08-20 10:10:14 -0500

    Add text input and display functionality

--- app.py
+++ app.py
@@ -3,3 +3,5 @@ from PIL import Image
 st.title("Hola, mi nombre es Juan Pablo GQ")
 image = Image.open('BobEsponja.png')
 st.image(image, caption = 'Interfaces Multimodales')
+texto = st.text_input('escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)

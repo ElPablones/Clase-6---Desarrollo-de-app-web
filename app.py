6d9d719dac928cc34a161984e41c2236ecaa0787
ElPablones <79945710+ElPablones@users.noreply.github.com>
2026-08-20 10:02:25 -0500

    Add image display to Streamlit app

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 import streamlit as st
+from PIL import Image
 st.title("Hola, mi nombre es Juan Pablo Guti Q")
+Image = Image.open('SpongeBob-and-Friends-vector-PNG.png')
+st.image(image, caption = 'Interfaces Multimodales')
